f93060d1570cdbe516e60739cd9a683fb3cc99e9
anhle1476 <anhle1476@gmail.com>
2020-10-17 11:57:19 +0700

    fix sound lag bug

--- js/controller/audioController/AudioController.js
+++ js/controller/audioController/AudioController.js
@@ -1,24 +1,24 @@
 export default class AudioController {
-  constructor(audio) {
-    this.audio = audio;
+  constructor(buffer) {
+    this.audioCtx = new AudioContext();
+    this.buffer = buffer;
   }
 
   playSong(name) {
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    const source = audioCtx.createBufferSource();
-    source.buffer = this.audio[name];
-    source.connect(audioCtx.destination);
+    const source = this.audioCtx.createBufferSource();
+    source.buffer = this.buffer[name];
+    source.connect(this.audioCtx.destination);
     source.start(0);
     return source;
   }
 
   playTheme() {
-    this.themeSong = this.playSong("theme");
-    this.themeSong.loop = true;
+    this.theme = this.playSong("theme");
+    this.theme.loop = true;
   }
 
   stopTheme() {
-    this.themeSong.stop();
+    this.theme.stop();
   }
 
   playJump() {
